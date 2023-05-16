36e1fd8c6059a22ee7da5c8b663a95f959956f0e
mtemel <mmtemel41@gmail.com>
2023-05-17 02:29:22 +0300

    quit seg solved

--- cmds/quit.cpp
+++ cmds/quit.cpp
@@ -29,7 +29,7 @@ void Server::quit(std::string str, int fd)
 				}
 			}
 		}
-		return;
+		// return;
 	}
 
 	std::vector<std::string> my_vec;
@@ -46,7 +46,7 @@ void Server::quit(std::string str, int fd)
 	}
 
 	i = 0;
-	bool flag = false;
+	bool _flag = false;
 	std::cout<<"QUIT MY_VEC SIZE: *"<<my_vec.size()<<"*\n";
 	if (my_vec.size() > 1 && my_vec[1][0] == ':')
 		my_vec[1] = my_vec[1].substr(1); //cut the :
@@ -55,22 +55,50 @@ void Server::quit(std::string str, int fd)
 	{
 		if (this->channels_[i].getchannelName() == my_vec[1])
 		{
-			flag = true;
+			_flag = true;
 			break;
 		}
 		i++;
 	}
 
-	if (this->client_ret(fd) && flag == true)
+	if (this->client_ret(fd) && (_flag == true || this->flag == 1))
 	{
 		if(this->client_ret(fd))
-			std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message\033[0m" << std::endl;
+			std::cout << "\033[1;91m" << this->client_ret(fd)->getNickName() << " is leaving with message22222\033[0m" << std::endl;
 
 		// Get index of the client to remove
-		unsigned int index = 0;
-		for (; index < this->clients_.size(); index++)
-			if (this->clients_[index].getFd() == fd)
-				break;
+		// unsigned int index = 0;
+		// for (; index < this->clients_.size(); index++)
+		// {
+		// 	if (this->clients_[index].getFd() == fd)
+		// 		break;
+		// }
+		// std::cout<<"FLAG 1, INDEX: *"<<index<<"*\n";
+
+		//send quit message
+		// std::string msg = ":" + this->clients_[index].getPrefixName() + " QUIT :Quit: " + str;
+		// send(fd, msg.c_str(), msg.size(), 0);
+		// Remove the client from the vector and close the connection
+		// this->clients_.erase(this->clients_.begin() + index);
+		//close(fd);
+
+		for (size_t i = 0; i < pollfds.size(); i++)
+		{
+			if(fd == pollfds[i].fd)
+			{
+				std::string b = ":" + this->client_ret(fd)->getPrefixName() + " QUIT " + str + "\r\n";
+				send(fd, b.c_str(), b.size(), 0);
+				close(pollfds[i].fd);
+				pollfds.erase(pollfds.begin()+i);
+			}
+		}
+		for (size_t i = 0; i < pollfds.size(); i++)
+		{
+			if(fd == clients_[i].getFd())
+			{
+				clients_.erase(clients_.begin()+i);
+			}
+		}
 
 		// Set the admin to the previous client if the current admin quits
 		unsigned int x = 0;
@@ -85,7 +113,7 @@ void Server::quit(std::string str, int fd)
 					{
 						this->channels_[x].setchannelAdminFd(-1);
 						this->channels_[x]._clientsFd.erase(this->channels_[x]._clientsFd.begin() + k); // Kanaldan kullanıcıyı sil
-						this->channels_[x]._clientsFd.pop_back();
+						// this->channels_[x]._clientsFd.pop_back(); //wtf
 						this->channels_.erase(this->channels_.begin() + x);
 						std::cout << "Burada mi3" << std::endl;
 						break;
@@ -106,16 +134,18 @@ void Server::quit(std::string str, int fd)
 			}
 			x++;
 		}
-
-		std::string msg = ":" + this->clients_[index].getPrefixName() + " QUIT :Quit: " + str;
-		send(fd, msg.c_str(), msg.size(), 0);
-		// Remove the client from the vector and close the connection
-		this->clients_.erase(this->clients_.begin() + index);
-		//close(fd);
 	}
-	else
+	// else
+	// {
+		// std::cerr << "\033[1;91mError: QUIT\033[0m" << std::endl;
+		// return;
+	// }
+	my_vec.clear();
+	std::vector<Client>::iterator ite = this->clients_.end();
+	for(std::vector<Client>::iterator it = this->clients_.begin(); it != ite; it++)
 	{
-		std::cerr << "\033[1;91mError: QUIT\033[0m" << std::endl;
-		return;
+		std::cout<<"class_attr:"<<(*it).getFd()<<","<<(*it).getUserName()<<","<<(*it).getHostName()<<",";
+		std::cout<<(*it).getServername()<<","<<(*it).getReelName()<<","<<(*it).getNickName()<<","<<std::endl;
 	}
+	std::cout<<"QUIT sonu geldi"<<std::endl;
 }
